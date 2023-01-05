c74f7f570cd89c7247ada709ca88077bc09f845a
Rabi Dey <cmc.rabi@gmail.com>
2023-01-05 23:23:02 +0530

    Added login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
 import { AuthService } from "../auth.service";
 
 @Component({
@@ -10,14 +11,21 @@ import { AuthService } from "../auth.service";
 
 export class LoginComponent implements OnInit{
   loginFromGroup: FormGroup|any;
+  private authListenerSubs: Subscription|any;
+  userIsAuthenticated = false;
   constructor(private authService: AuthService){}
   ngOnInit(): void {
-    console.log('Login Component');
+    this.userIsAuthenticated = this.authService.getIsAuth();
     this.loginFromGroup = new FormGroup({
       email: new FormControl('',
       [Validators.required,
       Validators.email]),
       password: new FormControl('', Validators.required)
+    });
+    this.authListenerSubs = this.authService
+    .getAuthStatusListener()
+    .subscribe( isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
     });
   }
 
@@ -29,5 +37,14 @@ export class LoginComponent implements OnInit{
       return;
     }
     this.authService.login(form.controls['email'].value, form.controls['password'].value);
+  }
+
+  onLogout()
+  {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
   }
 }
